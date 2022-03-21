e95e1690cb8eccc976f3697030c6a1180eebc8eb
mmar80 <m@502.io>
2022-03-21 12:22:33 -0500

    tested and fixed

--- pi/run_on_boot.sh
+++ pi/run_on_boot.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
+#This is an example for a script that runs on boot and logs the output of the referenced script 
+#or any errors encountered when running. Add the absolute path to script to crontab file (crontab -e):
+#@reboot /home/ops/repos/toolbox/pi/run_on_boot.sh &
+#remember to make the script executable with chmod u+x run_on_boot.sh
+
 # absolute path to virtual env py interpreter
 PYTHON=/home/ops/repos/toolbox/venv/bin/python 
 
 # absolute path to py script
-SCRIPT=/home/ops/repos/toolbox/pi/gpio_package_check.py
+SCRIPT=/home/ops/repos/toolbox/pi/gpio_pkg_check.py
 
 # absolute path to log file
-LOG=/absolute/path/to/log/file.log
+LOG=/home/ops/repos/toolbox/pi/gpio_pkg_check.log
 
 echo -e "\n######STARTUP $(date)######\n" >> $LOG 
 $PYTHON $SCRIPT >> $LOG 2>&1
