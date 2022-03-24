3700b1f59f210fcaae825f5208d5bf6b7ce190bc
mmar80 <m@502.io>
2022-03-24 14:22:35 -0500

    more basics

---
+++ basics/basic_while_loops.py
@@ -0,0 +1,12 @@
+# the difference between an if and a while loop demonstrated
+spam = 0
+if spam < 5:
+  print('Hello world.')
+  spam = spam + 1
+
+print('\n')
+
+spam = 0
+while spam < 5:
+  print('Hello world.')
+  spam = spam + 1
